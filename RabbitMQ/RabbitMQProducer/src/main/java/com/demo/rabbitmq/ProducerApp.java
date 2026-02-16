6ef9d1cbf9ea4775254eb0ddf89d52b626c1a63f
Sahil161967 <sahilbajkamble@gmail.com>
2026-02-16 17:14:09 +0530

    Producer Application using RabbitMQ

---
+++ RabbitMQ/RabbitMQProducer/src/main/java/com/demo/rabbitmq/ProducerApp.java
@@ -0,0 +1,49 @@
+package com.demo.rabbitmq;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class ProducerApp {
+
+    private static final String QUEUE_NAME = "student_queue";
+
+    public static void main(String[] args) {
+
+        try {
+
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost("192.168.1.36");
+            factory.setUsername("appuser");
+            factory.setPassword("123456");
+
+            Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel();
+
+            channel.queueDeclare(
+                    QUEUE_NAME,
+                    false,
+                    false,
+                    false,
+                    null
+            );
+
+            String message = "Hello Student from Sahil";
+
+            channel.basicPublish(
+                    "",
+                    QUEUE_NAME,
+                    null,
+                    message.getBytes()
+            );
+
+            System.out.println(" Sent: " + message);
+
+            channel.close();
+            connection.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
